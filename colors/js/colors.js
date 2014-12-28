0ab2718a88c5af246ff5e9f98a2f6a5266a49464
derv82 <derv82@gmail.com>
2014-12-27 23:48:53 -0800

    Fixing colors game

--- colors/js/colors.js
+++ colors/js/colors.js
@@ -6,6 +6,10 @@ function init() {
 	context.fillStyle = 'rgb(255,0,0)';
 	context.strokeStyle = 'rgb(255,0,0)';
 	context.lineWidth = 10;
+	$(document).on('swipeleft swiperight', '.selector', function(event) {
+		event.stopPropagation();
+		event.preventDefault();
+	});
 }
 
 function resize() {
@@ -14,6 +18,7 @@ function resize() {
 	$('.colorChooser').height( $(window).height() / $('.colorChooser').size());
 }
 
+var asdf = null;
 function main() {
 	init();
 
@@ -22,23 +27,49 @@ function main() {
 	$canvas.bind('touchstart mousedown', function(evt) {
 		context.lineWidth = 10;
 		context.beginPath();
-		context.moveTo(evt.clientX - 5, evt.clientY);
-		context.lineTo(evt.clientX + 5, evt.clientY);
+		var x = evt.pageX - canvas.offsetLeft,
+		    y = evt.pageY - canvas.offsetTop;
+		context.moveTo(x - 5, y);
+		context.lineTo(x + 5, y);
 		context.stroke();
-		context.moveTo(evt.clientX, evt.clientY);
-		return false;
+		context.moveTo(x, y);
+		asdf = true;
 	});
 
-	$canvas.bind('touchmove mousemove', function(evt) {
-		if (evt.buttons != 1) {
+	$canvas.bind('mousemove', function(evt) {
+		if (asdf == null) {
 			return;
 		}
-		context.lineTo(evt.clientX, evt.clientY);
+		var x = evt.pageX - canvas.offsetLeft,
+		    y = evt.pageY - canvas.offsetTop;
+		context.lineTo(x, y);
 		context.stroke();
-		context.moveTo(evt.clientX, evt.clientY);
+		context.moveTo(x, y);
 	});
 
-	$canvas.bind('touchend mouseup', function(evt) {
+	$canvas.bind('touchmove', function(evt) {
+		if (asdf == null) {
+			return;
+		}
+		if (!evt) {
+			var evt = event;
+		}
+		evt.preventDefault();
+		if (evt.originalEvent) {
+			evt = evt.originalEvent;
+		}
+		if (evt.targetTouches) {
+			evt = evt.targetTouches[0];
+		}
+		var x = evt.pageX - canvas.offsetLeft,
+		    y = evt.pageY - canvas.offsetTop;
+		context.lineTo(x, y);
+		context.stroke();
+		context.moveTo(x, y);
+	});
+
+	$canvas.bind('touchend touchcancel mouseup', function(evt) {
+		asdf = null;
 		context.closePath();
 	});
 
