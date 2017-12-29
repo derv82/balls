05f63b6ebd11b0cae59cfea37684020f05e23c73
derv82 <derv82@gmail.com>
2017-12-28 23:09:11 -0800

    Setup /maze, link from main menu.
    iOS support!

--- maze/maze.js
+++ maze/maze.js
@@ -1,6 +1,6 @@
-const Canvas = (function() {
-  const canvas = document.getElementById('canvas');
-  const context = canvas.getContext('2d');
+var Canvas = (function() {
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
 
   return { context: context, width: width, height: height, resize: resize, on: on };
 
@@ -22,7 +22,7 @@ const Canvas = (function() {
   }
 })();
 
-const Color = {
+var Color = {
   background: '#000',
   wall: '#333',
   road: '#ccc',
@@ -32,11 +32,11 @@ const Color = {
 };
 
 function Maze(width, height) {
-  const cells = new Array(width);
-  const path = [[0,1]];
-  let positionX;
-  let positionY;
-  let won;
+  var cells = new Array(width);
+  var path = [[0,1]];
+  var positionX;
+  var positionY;
+  var won;
 
   reset();
   resize();
@@ -45,26 +45,29 @@ function Maze(width, height) {
 
   function move(x, y) {
     if (won) return;
-    const cellWidth = parseInt(Canvas.width() / width);
-    const cellHeight = parseInt(Canvas.height() / height);
-    const cellX = parseInt(x / cellWidth);
-    const cellY = parseInt(y / cellHeight);
-    if (path.find(([x,y]) => (cellX === x && cellY === y))) {
+    var cellWidth = parseInt(Canvas.width() / width);
+    var cellHeight = parseInt(Canvas.height() / height);
+    var cellX = parseInt(x / cellWidth);
+    var cellY = parseInt(y / cellHeight);
+    if (path.find(function(arr) { return (cellX === arr[0] && cellY === arr[1]); })) {
       while (true) {
-        let pathPair = path.pop();
+        var pathPair = path.pop();
         if (pathPair[0] === cellX && pathPair[1] === cellY) {
           break;
         }
       }
     } else {
-      const diffX = positionX > cellX ? positionX - cellX : cellX - positionX;
-      const diffY = positionY > cellY ? positionY - cellY : cellY - positionY;
+      var diffX = positionX > cellX ? positionX - cellX : cellX - positionX;
+      var diffY = positionY > cellY ? positionY - cellY : cellY - positionY;
       if (diffX >= 1 && diffY >= 1) {
-        throw new Error('Invalid move (' + cellX + ',' + cellY + ') too far from current position (' + positionX + ',' + positionY + ')');
+        return;
+        //throw new Error('Invalid move (' + cellX + ',' + cellY + ') too far from current position (' + positionX + ',' + positionY + ')');
       } else if (diffX > 1 || diffY > 1) {
-        throw new Error('Invalid move (' + cellX + ',' + cellY + ') too far from current position (' + positionX + ',' + positionY + ')');
+        return;
+        //throw new Error('Invalid move (' + cellX + ',' + cellY + ') too far from current position (' + positionX + ',' + positionY + ')');
       } else if (cells[cellX][cellY] !== 1) {
-        throw new Error('Invalid move (' + cellX + ',' + cellY + ') not a road');
+        return;
+        //throw new Error('Invalid move (' + cellX + ',' + cellY + ') not a road');
       }
     }
     positionX = cellX;
@@ -80,17 +83,16 @@ function Maze(width, height) {
   function win() {
     if (won) return;
     won = true;
-    setTimeout(() => {
-      reset();
+    setTimeout(function() {
+      reset(width + 2, height + 2);
     }, 2000);
 
     // Rainbow, from https://stackoverflow.com/a/37713923
-    const canvasWidth = Canvas.width();
-    const canvasHeight = Canvas.height();
-    let bars = 20, i = 0, radius = Math.min(canvasWidth, canvasHeight);
+    var canvasWidth = Canvas.width();
+    var canvasHeight = Canvas.height();
+    var bars = 20, i = 0, radius = Math.min(canvasWidth, canvasHeight);
     Canvas.context.lineWidth = radius / 50;
     for (i = 0; i < bars; i++, radius -= Canvas.context.lineWidth - 1) { // increase bar, reduce radius
-      console.log('drawing line', i);
       Canvas.context.beginPath();
       Canvas.context.arc(canvasWidth * 0.5, canvasHeight, radius, 0, Math.PI, true); // half circle
       Canvas.context.strokeStyle = "hsl(" + (i / bars * 300) + ",90%,50%,50%)";  // set color using HSL
@@ -103,17 +105,22 @@ function Maze(width, height) {
     draw();
   }
 
-  function reset() {
-    let x, y;
+  function reset(newWidth, newHeight) {
+    var x, y;
+    if (newWidth !== undefined && newHeight !== undefined) {
+      width = newWidth;
+      height = newHeight;
+    }
 
     won = false;
     positionX = 0;
     positionY = 1;
     while (path.length > 1) path.pop();
+    while (cells.length > 0) cells.pop();
 
-    for (x = 0; x < cells.length; x++) {
-      cells[x] = new Array(height);
-      for (y = 0; y < cells[x].length; y++) {
+    for (x = 0; x < width; x++) {
+      cells.push(new Array(height));
+      for (y = 0; y < height; y++) {
         cells[x][y] = 0;
       }
     }
@@ -122,10 +129,10 @@ function Maze(width, height) {
   }
 
   function draw() {
-    const cellWidth = parseInt(Canvas.width() / width);
-    const cellHeight = parseInt(Canvas.height() / height);
+    var cellWidth = parseInt(Canvas.width() / width);
+    var cellHeight = parseInt(Canvas.height() / height);
 
-    let x, y, cellX, cellY;
+    var x, y, cellX, cellY;
 
     Canvas.context.fillStyle = Color.background;
     Canvas.context.fillRect(0, 0, Canvas.width(), Canvas.height());
@@ -148,24 +155,24 @@ function Maze(width, height) {
   }
 
   function _drawPath() {
-    const cellWidth = parseInt(Canvas.width() / width);
-    const cellHeight = parseInt(Canvas.height() / height);
-    let x, y;
-    path.forEach(([cellX, cellY], index) => {
+    var cellWidth = parseInt(Canvas.width() / width);
+    var cellHeight = parseInt(Canvas.height() / height);
+    var x, y;
+    path.forEach(function(arr, index) {
       if (index === path.length - 1) {
         Canvas.context.fillStyle = Color.cursor;
       } else {
         Canvas.context.fillStyle = Color.path;
       }
-      x = cellX * cellWidth;
-      y = cellY * cellHeight;
+      x = arr[0] * cellWidth;
+      y = arr[1] * cellHeight;
       Canvas.context.fillRect(x, y, cellWidth, cellHeight);
     });
   }
 
   function _buildMaze() {
-    const moves = [];
-    let x = 0, y = 1, directions, direction, movePair;
+    var moves = [];
+    var x = 0, y = 1, directions, direction, movePair;
     cells[x][y] = 1;
     while (x !== width || y !== height) {
       directions = [];
@@ -206,23 +213,38 @@ function Maze(width, height) {
   }
 }
 
-const maze = Maze(8, 6);
+var maze = Maze(8, 6);
 
 window.addEventListener('resize', maze.resize);
 
-let dragging = false;
+var dragging = false;
 
-Canvas.on('mousedown', event => {
-  maze.move(event.clientX, event.clientY);
+function mousedown(event) {
   dragging = true;
-}, true);
+  maze.move(event.pageX, event.pageY);
+}
 
-Canvas.on('mousemove', event => {
+function mouseup(event) {
+  dragging = false;
+}
+
+function mousemove(theEvent) {
+  var evt = theEvent || event;
   if (dragging) {
-    maze.move(event.clientX, event.clientY);
+    evt.preventDefault();
+    if (evt.targetTouches && evt.targetTouches.length > 0) {
+      evt = evt.targetTouches[0];
+    }
+    maze.move(evt.pageX, evt.pageY);
   }
-}, true);
+}
 
-Canvas.on('mouseup', event => dragging = false);
+Canvas.on('mousedown', mousedown, true);
+Canvas.on('touchstart', mousedown, true);
 
-Canvas.on('selectstart', event => {e.preventDefault(); return false; }, false);
+Canvas.on('mousemove', mousemove);
+Canvas.on('touchmove', mousemove);
+
+Canvas.on('mouseup', mouseup);
+Canvas.on('touchend', mouseup);
+Canvas.on('touchcancel', mouseup);
